a0678769c2766f425cb66c517e9e6342c9874f2d
nathanKith <ntolparov@gmail.com>
2021-03-11 20:08:21 +0300

    100% coverage

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Order from './Order';
+import {fakeOrders} from '../data/fakeOrders';
+
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.js', () => {
-  it('some test', () => {
+  it('render with fakeOrders[0]', () => {
+    const data = {
+      order: fakeOrders[0],
+    };
 
+    const wrapper = shallow(<Order {...data}/>);
+
+    expect(getDate).toHaveBeenCalledTimes(1);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('render with null props in data', () => {
+    const data = {
+      order: {
+        shop: null,
+        date: null,
+      },
+    };
+
+    const result = shallow(<Order {...data}/>);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('render with items null in data', () => {
+    const data = {
+      order: {
+        shop: 'asa',
+        date: 1,
+        items: null,
+      },
+    };
+
+    const result = shallow(<Order {...data}/>);
+
+    expect(getDate).toHaveBeenCalledTimes(1);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('render with null props', () => {
+    const data = {
+      order: null,
+    };
+
+    const result = shallow(<Order {...data}/>);
+
+    expect(result).toMatchSnapshot();
   });
 });
 
